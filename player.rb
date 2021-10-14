ddc76bddda8ae38d457d039acb27fd644062728a
Wahab Alamin <wahabalamin@gmail.com>
2021-10-14 16:57:14 -0400

    add into Player class the ability to set a name for player when initialized and a method to reduce lifepoints

--- player.rb
+++ player.rb
@@ -1,3 +1,23 @@
 class Player
-  x
+
+  attr_accessor :life
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @life = 3
+  end
+
+  def life_drop
+    @life -= 1
+    puts "Seriously?! No!"
+
+    if self.life == 0
+      puts "#{name} is now dead"
+    else
+      puts "Your new life is #{life}/3"
+    end
+
+  end
+
 end
