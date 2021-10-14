97ab2b86dce50c53d7643082f090d909243b1eee
Wahab Alamin <wahabalamin@gmail.com>
2021-10-14 18:39:11 -0400

    Remove rand_num method as there isn’t a use for it anymore. Add new method new_question to ask player what the sum of sums is

--- questions.rb
+++ questions.rb
@@ -7,13 +7,12 @@ class Questions
     @second = rand(1..10)
   end
 
-  def rand_num
-    @first = rand(1..10)
-    @second = rand(1..10)
-  end
-
   def num_answer
     self.first + self.second
+  end
+
+  def new_question
+    puts "What is #{@first} + #{@second}?"
   end
 
 end
