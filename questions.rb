8fbd36c3dc6a40299115be608c62e057990c6319
Wahab Alamin <wahabalamin@gmail.com>
2021-10-14 17:00:01 -0400

    add conductor which sets random numbers to 2 instance variables, method which sets 2 new random numbers, and method that contains the answer to the sum of both numbers

--- questions.rb
+++ questions.rb
@@ -1,3 +1,19 @@
 class Questions
-  x
+
+  attr_reader :first, :second
+
+  def initialize
+    @first = rand(1..10)
+    @second = rand(1..10)
+  end
+
+  def rand_num
+    @first = rand(1..10)
+    @second = rand(1..10)
+  end
+
+  def num_answer
+    self.first + self.second
+  end
+
 end
